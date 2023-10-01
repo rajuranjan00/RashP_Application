85c0daa63644a0727c1c686f53bb09979351819a
Rishabh Singh <rishabh15112001@gmail.com>
2023-10-01 18:08:01 +0530

    Deployment setup

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,9 @@ export const getServerSideProps = async (ctx) => {
     admin = true;
   }
 
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get(
+    "https://rash-p-application-wine.vercel.app/api/products"
+  );
   return {
     props: {
       pizzaList: res.data,
